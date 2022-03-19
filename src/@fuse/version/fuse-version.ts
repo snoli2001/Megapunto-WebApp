9f31f5310658a41abbdd45fa5e4f813283939f8e
snoli2001 <sebastian.noli@gmail.com>
2022-03-19 18:33:04 -0500

    cleanup

--- src/@fuse/version/fuse-version.ts
+++
@@ -1,3 +0,0 @@
-import { Version } from '@fuse/version/version';
-
-export const FUSE_VERSION = new Version('14.1.1').full;
--- src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/naming-convention */
-export const environment = {
-    production: true,
-    API_URL: 'https://api-megapunto-qa.sis360.com.pe/api',
-    URL_API_TOKEN_ACCESS:
-        'https://api-megapunto-qa.sis360.com.pe/api/Auth/token',
-    API_TOKEN_KEY_PASS: 'PK_MEGAPUNTO',
-    API_TOKEN_KEY_SECRET: 'U7pkd5SQ',
-};
