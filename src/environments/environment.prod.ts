d83a8953d468f7b664cc4f9a2994c4bf43ab8728
snoli2001 <sebastian.noli@gmail.com>
2022-03-19 18:17:59 -0500

    prod config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 export const environment = {
-    production: true
+    production: true,
+    API_URL: 'https://api-megapunto-qa.sis360.com.pe/api',
+    URL_API_TOKEN_ACCESS:
+        'https://api-megapunto-qa.sis360.com.pe/api/Auth/token',
+    API_TOKEN_KEY_PASS: 'PK_MEGAPUNTO',
+    API_TOKEN_KEY_SECRET: 'U7pkd5SQ',
 };
