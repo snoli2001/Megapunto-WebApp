028fcd4554c8445e3f22ebb1e90acd11a7e08ba7
snoli2001 <sebastian.noli@gmail.com>
2022-09-20 17:59:19 -0500

    Actualizacion de URLS y primera version del encripted

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,11 +2,11 @@
 
 export const environment = {
     production: true,
-    API_URL: 'https://api-megapunto-qa.sis360.com.pe/api',
+    API_URL: 'https://api-megapunto-qa.sis360.com.pe/web/prd/mp',
     URL_API_TOKEN_ACCESS:
-        'https://api-megapunto-qa.sis360.com.pe/api/Auth/token',
-    API_TOKEN_KEY_PASS: 'PK_MEGAPUNTO',
-    API_TOKEN_KEY_SECRET: 'U7pkd5SQ',
+        'https://api-megapunto-qa.sis360.com.pe/web/prd/mp/auth/token',
+    API_TOKEN_KEY_PASS: 'PK_MEGAPUNTO_WEB',
+    API_TOKEN_KEY_SECRET: 'sz*Cs1va918W',
     GOOGLE_SECRET_KEY: 'AIzaSyDVRcmeuzxMHmE4frC6_vWPzrp4m_UnQTw',
     // GOOGLE_SECRET_KEY: 'AIzaSyCNagAMyh4X7_h3y3D_i0iVAUu-duLc6RQ',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,10 +5,11 @@
 
 export const environment = {
     production: false,
-    API_URL: 'https://api-megapunto-qa.sis360.com.pe/api',
-    URL_API_TOKEN_ACCESS: 'https://api-megapunto-qa.sis360.com.pe/api/Auth/token',
-    API_TOKEN_KEY_PASS: 'PK_MEGAPUNTO',
-    API_TOKEN_KEY_SECRET: 'U7pkd5SQ',
-    GOOGLE_SECRET_KEY: 'AIzaSyDVRcmeuzxMHmE4frC6_vWPzrp4m_UnQTw'
-    // GOOGLE_SECRET_KEY: 'AIzaSyCNagAMyh4X7_h3y3D_i0iVAUu-duLc6RQ'
+    API_URL: 'https://api-megapunto-qa.sis360.com.pe/web/prd/mp',
+    URL_API_TOKEN_ACCESS:
+        'https://api-megapunto-qa.sis360.com.pe/web/prd/mp/auth/token',
+    API_TOKEN_KEY_PASS: 'PK_MEGAPUNTO_WEB',
+    API_TOKEN_KEY_SECRET: 'sz*Cs1va918W',
+    GOOGLE_SECRET_KEY: 'AIzaSyDVRcmeuzxMHmE4frC6_vWPzrp4m_UnQTw',
+    // GOOGLE_SECRET_KEY: 'AIzaSyCNagAMyh4X7_h3y3D_i0iVAUu-duLc6RQ',
 };
